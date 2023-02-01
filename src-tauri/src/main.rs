2409f5a069c64fc4e8223cbd0b3c2fab22c8877c
polnio <polniolino@gmail.com>
2023-02-01 17:10:33 +0100

    added program selecting (persistant) and running

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,44 @@
     windows_subsystem = "windows"
 )]
 
+use std::process::Command;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn run_program(path: &str) {
+    println!("run program back");
+    if cfg!(target_os = "windows") {
+        println!("windows");
+        Command::new("cmd")
+            .args(["/C", path])
+            .spawn()
+            .expect("Failed to run the program. Check if the path is correct");
+    } else {
+        println!("not windows");
+        Command::new("sh")
+            .args(["-c", path])
+            .spawn()
+            .expect("Failed to run the program. Check if the path is correct");
+    }
+}
+
+#[tauri::command]
+fn log(message: &str, mode: Option<u8>) {
+    match mode.unwrap_or(0) {
+        0 => println!("LOG: {}", message),
+        1 => eprintln!("Error: {}", message),
+        _ => println!("LOG: {}", message),
+    }
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, run_program, log])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
+import { open } from "@tauri-apps/api/dialog";
 import { invoke } from "@tauri-apps/api/tauri";
+import { appWindow } from "@tauri-apps/api/window";
+import { SettingsManager } from "tauri-settings";
+
+type Settings = {
+  programPath: string;
+};
+
+const settingsManager = new SettingsManager<Settings>(
+  {
+    programPath: "",
+  },
+  {
+    fileName: "settings",
+  }
+);
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
+let programPathEl: HTMLSpanElement | null;
+let runButton: HTMLButtonElement | null;
+let selectProgramButton: HTMLButtonElement | null;
 
 async function greet() {
   if (greetMsgEl && greetInputEl) {
@@ -12,9 +31,65 @@ async function greet() {
   }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+async function runProgram() {
+  const programPath = await settingsManager.get("programPath");
+
+  if (programPath.length === 0) {
+    return;
+  }
+  await invoke("run_program", {
+    path: programPath,
+  });
+  appWindow.close();
+}
+
+async function selectProgram() {
+  const path = await open({
+    multiple: false,
+  });
+  if (path === null) {
+    return;
+  }
+  if (Array.isArray(path)) {
+    if (path.length === 0) {
+      saveProgramPath(path[0]);
+    }
+    return;
+  }
+  saveProgramPath(path);
+}
+
+async function saveProgramPath(path: string) {
+  if (programPathEl !== null) {
+    try {
+      await settingsManager.set("programPath", path);
+      programPathEl.textContent = path;
+    } catch (error) {
+      programPathEl.textContent = "Une erreur est survenue";
+    }
+  }
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const settings = await settingsManager.initialize();
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
+  programPathEl = document.querySelector<HTMLSpanElement>("#program-path");
+  runButton = document.querySelector<HTMLButtonElement>("button#run");
+  selectProgramButton = document.querySelector<HTMLButtonElement>(
+    "button#select-program"
+  );
+
+  if (programPathEl !== null) {
+    programPathEl.textContent =
+      settings.programPath.length > 0
+        ? settings.programPath
+        : "Aucun programme sélectionné";
+  }
+
+  runButton?.addEventListener("click", runProgram);
+  selectProgramButton?.addEventListener("click", selectProgram);
+
   document
     .querySelector("#greet-button")
     ?.addEventListener("click", () => greet());
